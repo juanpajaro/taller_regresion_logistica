c103f28decd5e9e24964799c0215b1321665965c
juan <juanpajaro@javeriana.edu.co>
2025-08-10 17:27:34 -0500

    se agregaron las funciones de limpiar_tweet y construir frecuencias

--- sol.py
+++ sol.py
@@ -2,8 +2,14 @@
 import nltk
 nltk.download('twitter_samples')
 nltk.download('stopwords')
+
 from nltk.corpus import twitter_samples
 import numpy as np
+from nltk.tokenize import TweetTokenizer
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer
+import re
+import string
 
 def cargar_datos():
     
@@ -26,6 +32,57 @@ def balancear_tweets(positivos, negativos):
 
     return train_x, train_y, test_x, test_y
 
+# Función para limpiar los tweets, eliminando URLs, menciones y caracteres especiales, también calcula el stemming y elimina stopwords
+def limpiar_tweet(tweet):
+    """Funcion para limpiar un tweet eliminando URLs, menciones, caracteres especiales y números,
+    además de aplicar stemming y eliminar stopwords.
+    Args:
+        tweet (str): El tweet a limpiar.
+    Returns:
+        list: Lista de palabras limpias del tweet."""
+
+    stemmer = PorterStemmer()
+    stop_words = set(stopwords.words('english'))    
+
+    # Eliminar simbolos de monetización
+    tweet = re.sub(r'\$\w*', '', tweet)
+    # Eliminar URLs
+    tweet = re.sub(r'https?://[^\s\n\r]+', '', tweet)
+    # Eliminar menciones
+    tweet = re.sub(r'@\w+', '', tweet)
+    # Eliminar caracteres especiales y números
+    tweet = re.sub(r'[^a-zA-Z\s]', '', tweet)
+
+    # Tokenizar el tweet
+    tokenizer = TweetTokenizer(preserve_case=False, strip_handles=True, reduce_len=True)
+    tweet_tokens = tokenizer.tokenize(tweet)
+
+    tweets_limpios =[]
+    for palabra in tweet_tokens:
+        if (palabra not in stop_words and palabra not in string.punctuation):
+            # Aplicar stemming
+            palabra_stemmed = stemmer.stem(palabra)
+            tweets_limpios.append(palabra_stemmed)
+
+    return tweets_limpios
+
+# Función para construir las frecuencias de palabras acorde a la etiqueta, limpia los trinos, debe retornar un diccionario
+def construir_frecuencias(tweets, etiquetas):
+
+    yslista = np.squeeze(etiquetas).tolist()
+    
+    frecuencias = {}
+    for y, tweet in zip(yslista, tweets):
+        for palabra in limpiar_tweet(tweet):
+            pair = (y, palabra)
+            if pair not in frecuencias:
+                frecuencias[pair] = +1
+            else:
+                frecuencias[pair] = 1
+        
+    return frecuencias
+
+
 def main():
     positivos, negativos = cargar_datos()
     print(f"Número de tweets positivos: {len(positivos)}")
@@ -35,7 +92,12 @@ def main():
     print(f"Número de tweets de entrenamiento: {len(train_x)}")
     print(f"Número de tweets de prueba: {len(test_x)}")
     print(f"Primer tweet de entrenamiento: {train_x[0]}")
-    
+    print("tipo train_x:", type(train_x[0]))
+    print("tipo train_y:", type(train_y[0]))
+
+    frecuencias = construir_frecuencias(train_x, train_y)
+    print(f"Número de palabras únicas en el vocabulario: {len(frecuencias)}")
+
 if __name__ == "__main__":
     main()
 # Asegúrate de tener NLTK instalado: pip install nltk
